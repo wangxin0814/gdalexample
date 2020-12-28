c1f0ce8845e41e50c57c404bd5640e94df526ae0
汪鑫 <wangxin2@diit.cn>
2020-12-28 15:56:56 +0800

    Shapefile 读取与生成

--- mian.go
+++ mian.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"github.com/lukeroth/gdal"
 	"image"
 	"image/color"
@@ -9,6 +11,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 // 栅格数据转WGS84
@@ -61,8 +64,6 @@ func Vector2WGS84(infile, outfile string) error {
 	projection := gdal.CreateSpatialReference(string(b))
 	space, _ := projection.AttrValue("AUTHORITY", 1)
 	if !strings.EqualFold(space, "4326") {
-		os.Setenv("SHAPE_ENCODING", "UTF-8")
-
 		sds, err := gdal.OpenEx(infile, gdal.OFVector, nil, nil, nil)
 		if err != nil {
 			log.Fatal("Open Infile Error")
@@ -84,12 +85,13 @@ func Vector2WGS84(infile, outfile string) error {
 
 // 生成影像的缩略图
 func Thumb(infile, outimg string) error {
+	fmt.Println("start open", time.Now())
 	ds, err := gdal.Open(infile, gdal.ReadOnly)
 	if err != nil {
 		log.Fatal("Open Infile Error")
 		return err
 	}
-
+	fmt.Println("end open", time.Now())
 	defer ds.Close()
 
 	f, err := os.Create(outimg)
@@ -103,6 +105,7 @@ func Thumb(infile, outimg string) error {
 	xSize := ds.RasterXSize()
 	ySize := ds.RasterYSize()
 
+	fmt.Println("band num", num)
 	if num < 3 { // 单波段
 		img := image.NewGray(image.Rect(0, 0, 100, 100))
 
@@ -143,13 +146,14 @@ func Thumb(infile, outimg string) error {
 		blue := ds.RasterBand(3)
 		blueMin, blueMax := blue.ComputeMinMax(2)
 
+		fmt.Println("start read", time.Now())
 		redBuffer := make([]int32, 100*100)
 		red.IO(gdal.Read, 0, 0, xSize, ySize, redBuffer, 100, 100, 0, 0)
 		greenBuffer := make([]int32, 100*100)
 		green.IO(gdal.Read, 0, 0, xSize, ySize, greenBuffer, 100, 100, 0, 0)
 		blueBuffer := make([]int32, 100*100)
 		blue.IO(gdal.Read, 0, 0, xSize, ySize, blueBuffer, 100, 100, 0, 0)
-
+		fmt.Println("end read", time.Now())
 		for y := 0; y < 100; y++ {
 			for x := 0; x < 100; x++ {
 				r := redBuffer[y*100+x]
@@ -175,6 +179,81 @@ func Thumb(infile, outimg string) error {
 	return nil
 }
 
+func ReadShapefile(infile string) error {
+	driver := gdal.OGRDriverByName("ESRI Shapefile")
+	ds, ok := driver.Open(infile, 0)
+	if !ok {
+		log.Fatal("Open Shapefile Error")
+		return errors.New("Open Shapefile Error")
+	}
+	defer ds.Destroy()
+
+	layer := ds.LayerByIndex(0)
+
+	for {
+		if feature := layer.NextFeature(); feature != nil {
+			fmt.Println("FID: ", feature.FID())
+			df := feature.Definition()
+			for i := 0; i < df.FieldCount(); i++ {
+				attr := df.FieldDefinition(i).Name() // 属性字段名称
+				switch df.FieldDefinition(i).Type() {
+				case gdal.FT_String:
+					fmt.Printf("%s : %s", attr, feature.FieldAsString(i))
+				case gdal.FT_Integer:
+					fmt.Printf("%s : %d", attr, feature.FieldAsInteger(i))
+				case gdal.FT_Real:
+					fmt.Printf("%s : %f", attr, feature.FieldAsFloat64(i))
+				}
+			}
+
+			geometry, _ := feature.Geometry().ToWKT()
+			fmt.Println("Geometry: ", geometry)
+		} else {
+			break
+		}
+	}
+
+	return nil
+}
+
+func WriteShapefile(outfile string) error {
+	driver := gdal.OGRDriverByName("ESRI Shapefile")
+
+	ds, ok := driver.Create(outfile, nil)
+	if !ok {
+		log.Fatal("Create Shapefile Error")
+		return errors.New("Create Shapefile Error")
+	}
+	defer ds.Destroy()
+	ref := gdal.CreateSpatialReference("")
+	ref.FromEPSG(4326)
+
+	layer := ds.CreateLayer("", ref, gdal.GT_Unknown, nil)
+
+	// fileds := []string{"OBJECTID", "YBBQ"}
+
+	ObjectID := gdal.CreateFieldDefinition("OBJECTID", gdal.FT_Integer64)
+	layer.CreateField(ObjectID, false)
+
+	YBBQ := gdal.CreateFieldDefinition("YBBQ", gdal.FT_String)
+	YBBQ.SetWidth(64)
+	layer.CreateField(YBBQ, false)
+
+	for i := 0; i < 100; i++ {
+		feature := layer.Definition().Create()
+		feature.SetFID(int64(i))
+		feature.SetFieldInteger64(0, int64(i*i))
+		feature.SetFieldString(1, "小麦")
+		geometry, _ := gdal.CreateFromWKT("POLYGON((119.226 35.7242,119.607 35.6541,119.521 35.343,119.141 35.413,119.226 35.7242))", ref)
+		feature.SetGeometry(geometry)
+		layer.Create(feature)
+		feature.Destroy()
+	}
+
+	return nil
+
+}
+
 func main() {
-	//...
+	// todo...
 }
